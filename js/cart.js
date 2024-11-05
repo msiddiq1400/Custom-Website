ec7c68f3d9932da5903b80806357e8659536aa15
Siddiq <70774749+msiddiq1400@users.noreply.github.com>
2024-11-05 15:49:34 +0500

    Revert "Extract cart"
    This reverts commit 209d212537927cfa823ca7d12ec1f51e901cc629.

--- js/cart.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.js-show-cart').on('click',function(){
-        $('.js-panel-cart').addClass('show-header-cart');
-    });
-    
-    $('.js-hide-cart').on('click',function(){
-        $('.js-panel-cart').removeClass('show-header-cart');
-    });
-});
--- js/main.js
+++ js/main.js
@@ -167,6 +167,22 @@
         }    
     });
 
+
+
+
+    /*==================================================================
+    [ Cart ]*/
+    $('.js-show-cart').on('click',function(){
+        $('.js-panel-cart').addClass('show-header-cart');
+    });
+
+    $('.js-hide-cart').on('click',function(){
+        $('.js-panel-cart').removeClass('show-header-cart');
+    });
+
+    /*==================================================================
+    [ Cart ]*/
+
     /*==================================================================
     [ +/- num product ]*/
     $('.btn-num-product-down').on('click', function(){
