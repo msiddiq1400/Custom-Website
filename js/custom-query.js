9bbf898c2ea70026c86bdf6a720174e0db2557b3
Siddiq <70774749+msiddiq1400@users.noreply.github.com>
2024-11-05 19:35:08 +0500

    Remove extra js files

--- js/custom-query.js
+++ js/custom-query.js
@@ -1,3 +1,26 @@
+// Cart
+$(document).ready(function () {
+    $('.js-show-cart').on('click', function () {
+        $('.js-panel-cart').addClass('show-header-cart');
+    });
+    
+    $('.js-hide-cart').on('click', function () {
+        $('.js-panel-cart').removeClass('show-header-cart');
+    });
+});
+
+// Side bar
+$(document).ready(function() {
+    $('.js-show-sidebar').on('click', function() {
+        $('.js-sidebar').addClass('show-sidebar');
+    });
+
+    $('.js-hide-sidebar').on('click', function() {
+        $('.js-sidebar').removeClass('show-sidebar');
+    });
+});
+
+// Mobile Menu
 $(document).ready(function() {
     // Toggle the mobile menu
     $('.btn-show-menu-mobile').on('click', function() {
--- js/sidebar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.js-show-sidebar').on('click', function() {
-        $('.js-sidebar').addClass('show-sidebar');
-    });
-
-    $('.js-hide-sidebar').on('click', function() {
-        $('.js-sidebar').removeClass('show-sidebar');
-    });
-});
